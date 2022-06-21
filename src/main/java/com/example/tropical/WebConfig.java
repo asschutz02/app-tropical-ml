71fe246d22de546a29bc77dac47f8f3bd8080db1
aschutz02 <arthurschutzdasilva@gmail.com>
2022-06-21 19:30:03 -0300

    update

--- src/main/java/com/example/tropical/WebConfig.java
+++ src/main/java/com/example/tropical/WebConfig.java
@@ -12,7 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-				.allowedOrigins("http://localhost:4200");
+				.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
 	}
 }
