946345e0a17d0ace6327a11f912587e646a2e16e
codebrain.arthur.schutz <codebrain.arthur.schutz@kiwigrid.com>
2023-03-23 13:13:19 -0300

    local version

--- src/main/java/com/example/tropical/selenium/email/EmailJavaSender.java
+++ src/main/java/com/example/tropical/selenium/email/EmailJavaSender.java
@@ -25,9 +25,9 @@ public class EmailJavaSender {
 	public static void emailJavaSender() {
 
 		//         Recipient's email ID needs to be mentioned.
-//		        String to = "arthur.schutz123@gmail.com";
+		        String to = "arthur.schutz123@gmail.com";
 		//        String to = \"vendas@tropicalimport.com.br";
-		String to = "marcasregistradas@tropicalimport.com.br";
+//		String to = "marcasregistradas@tropicalimport.com.br";
 
 		// Sender's email ID needs to be mentioned
 		String from = "arthurschutzdasilva@gmail.com";
